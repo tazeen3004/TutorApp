43473a2ad3bb4ca758957e403ac089444e239de9
tazeen khan <tazeenfirojkhan@gmail.com>
2017-03-26 14:19:46 +0530

    Login and sign up

--- TutorApp/ViewController.swift
+++ TutorApp/ViewController.swift
@@ -7,9 +7,66 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    @IBAction func login(_ sender: UIButton)
+    {
+        if emailText.text != "" && passwordText.text != ""
+        {
+            if segmentControl.selectedSegmentIndex == 0
+            {
+                FIRAuth.auth()?.signIn(withEmail: emailText.text!, password: passwordText.text!, completion: {(user,error) in
+                    if user != nil
+                    {
+                        print ("suh")
+                    }
+                    else
+                    {
+                        if let myError = error?.localizedDescription
+                        {
+                            print(myError)
+                        
+                        }
+                        else
+                        {
+                            print("error")
+                        }
+                    }
+                })
+            }
+            else
+            {
+                FIRAuth.auth()?.createUser(withEmail: emailText.text!, password: passwordText.text!, completion: { (user,error) in
+                    if user != nil
+                        
+                    {
+                        
+                    }
+                    else
+                    {
+                        if let myError = error?.localizedDescription
+                        {
+                            print(myError)
+                        }
+                        else
+                        {
+                            print("error")
+                        }
+                    }
+                })
+            }
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
